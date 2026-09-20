68a07ac280839f711e28c08da29ad9f5ced6e31e
Link2071 <Kshaaflink@gmail.com>
2026-09-20 18:30:16 +1000

    Create basic logic for moving to a given point and lay foundation for moving between the four corners

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    private Vector3[] _corners;
+    private Vector3 _endPos;
+    private Vector3 _startPos;
+    private float _startTime;
+    private float _movementDuration;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _corners = new Vector3[4];
+        _corners[0] = new Vector3(6.5f, -1.5f, 0);
+        _corners[1] = new Vector3(1.5f, -1.5f, 0);
+        _corners[2] = new Vector3(1.5f, -5.5f, 0);
+        _corners[3] = new Vector3(6.5f, -5.5f, 0);
+
+        _endPos = _corners[0];
+        _startPos = transform.position;
+        _startTime = Time.time;
+        _movementDuration = Vector3.Distance(transform.position, _endPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = (Time.time - _startTime) / _movementDuration;
+        transform.position = Vector3.Lerp(_startPos, _endPos, t);
+    }
+}
